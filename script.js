20a69c8845dca5dc5d7b4ae01022b5ffdddc7d86
Carlos <carlosaguinaga01@gmail.com>
2021-08-29 10:19:49 -0500

    feat: questions 1

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ const trivia_amount = document.getElementById("trivia_amount");
 const trivia_category = document.getElementById("trivia_category");
 const trivia_difficulty = document.getElementById("trivia_difficulty");
 const trivia_type = document.getElementById("trivia_type");
+const questionsContent = document.getElementById("questionsContent");
+
+const bnt1 = document.getElementById('btn1')
+const bnt2 = document.getElementById('btn2')
+const bnt3 = document.getElementById('btn3')
+const bnt4 = document.getElementById('btn3')
+
+
+// generales
+const triviaState = {
+  totalQuestions: null,
+  currentQuestion: 0,
+  correctAnswers: 0
+};
 
 // funciones
 const getCategories = async () => {
@@ -27,22 +41,56 @@ const getQuestions = async (params) => {
   const res = await fetch(url);
   const data = await res.json();
 
-  console.log(data)
+  startGame(data)
 };
+const setQuestion =(iterator) => {
+  const item = iterator.next();
+  if (item.done) {
+    // finalizar trivia
+    return null
+  }
 
-getCategories();
+  const question = item.value
+  document.getElementById("questionName").innerText = question.question;
+  if (question.type === 'boolean') {
+    btn1.innerText = "True";
+    btn2.innerText = "False";
+    btn3.style.display = "none";
+    btn4.style.display = "none";
+  }else{
+    btn1.style.display = "Block";
+    btn2.style.display = "Block";
+    btn3.style.display = "Block";
+    btn4.style.display = "Block";
+  }
+}
+const startGame = ({results}) => {
+  // ocutar configuración inicial
+  form.style.display = 'none'
+  questionsContent.style.display = 'flex'
+  // results[triviaState.currentQuestion].incorrect_answers.length === 1 // V o F
+
+  const iterator = results[Symbol.iterator]();
+    setQuestion(iterator)
+ 
+
+
+
+}
+
 
 // listeners
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
   const params = {};
   params.amount = trivia_amount.value;
   if (trivia_category.value !== "any") params.category = trivia_category.value;
-  if (trivia_difficulty.value !== "any")
-    params.difficulty = trivia_difficulty.value;
+  if (trivia_difficulty.value !== "any") params.difficulty = trivia_difficulty.value;
   if (trivia_type.value !== "any") params.type = trivia_type.value;
-
 
   getQuestions(params)
 });
+
+
+getCategories();
